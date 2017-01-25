430edd059c398f8c077d15e035043704a607beaa
OLDMANE\Oldmane <alexbatrinu13@yahoo.com>
2017-01-25 21:15:55 +0000

    Added new soundtrack for level 1, did half of level 2 design and added soundtrack.

---
+++ Assets/Scripts/Coffee.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coffee : MonoBehaviour {
+	
+	GameObject player;
+	public GameObject note;
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		note.SetActive(false);
+		panel.SetActive(false);
+		GameObject player = GameObject.Find("Character");
+		CharController CharController = player.GetComponent<CharController>();
+	
+	}
+	
+	void Smth(){
+		
+		StartCoroutine("Congrats");
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		GameObject player = GameObject.Find("Character");
+		CharController CharController = player.GetComponent<CharController>();
+		if (other.gameObject.tag == "Player"){
+			gameObject.renderer.enabled = false;
+			Smth();
+			audio.Play();	
+		}
+	
+		
+	}
+	
+	
+	
+	IEnumerator Congrats(){
+		
+		note.SetActive(true);
+		panel.SetActive(true);
+		
+		yield return new WaitForSeconds(5f);
+
+		note.SetActive(false);
+		panel.SetActive(false);	
+		Destroy(gameObject);
+		Application.LoadLevel("LoadingScreen2");
+	}
+	
+}
+	
