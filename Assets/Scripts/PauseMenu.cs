3f92838b363c8a264fec8966b6806ffc416e2057
OLDMANE\Oldmane <alexbatrinu13@yahoo.com>
2017-01-28 02:18:02 +0000

    Fixed some small issues with the design. Not huge changes from last push.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -30,6 +30,10 @@ public class PauseMenu : MonoBehaviour {
 		audio.Play ();
 	}
 	
+	public void Reload(){
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	
 	public void Quit(){
 		Application.Quit();	
 	}
