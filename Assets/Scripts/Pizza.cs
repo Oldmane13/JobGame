77cd64c9db4c61fbab52776f727afe8dd6d7faab
OLDMANE\Oldmane <alexbatrinu13@yahoo.com>
2017-01-28 18:55:52 +0000

    Level 1 and 2 Complete - added objective to level 2, added scripts for objective and also dialogue boxes.

---
+++ Assets/Scripts/Pizza.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pizza : MonoBehaviour {
+
+	GameObject player;
+	public GameObject note;
+	public GameObject panel;
+	public float waitTime;
+	
+	// Use this for initialization
+	void Start () {
+		note.SetActive(false);
+		panel.SetActive(false);
+		GameObject player = GameObject.Find("Character");
+		CharController CharController = player.GetComponent<CharController>();
+		
+	}
+	
+	void CatchPizza(){
+		
+		StartCoroutine("End");
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		GameObject player = GameObject.Find("Character");
+		CharController CharController = player.GetComponent<CharController>();
+		if (other.gameObject.tag == "Player"){
+			gameObject.renderer.enabled = false;
+			CatchPizza();
+			audio.Play();	
+		}
+		
+		
+	}
+	
+	
+	
+	IEnumerator End(){
+		
+		note.SetActive(true);
+		panel.SetActive(true);
+		
+		yield return new WaitForSeconds(waitTime);
+		
+		note.SetActive(false);
+		panel.SetActive(false);	
+		Destroy(gameObject);
+	}
+	
+}
---
+++ Assets/Scripts/TeleportToLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportToLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other){
+	
+		if(other.gameObject.tag == "Player"){
+			Application.LoadLevel("LoadingScreen3");
+		}
+	}
+}
