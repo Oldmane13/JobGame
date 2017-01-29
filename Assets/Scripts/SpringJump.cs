e6bf35d7582d1a2ae943c1a864fac19e5c18bacc
OLDMANE\Oldmane <alexbatrinu13@yahoo.com>
2017-01-29 02:10:28 +0000

    Started work on level 3, added spring to level 1.

---
+++ Assets/Scripts/SpringJump.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringJump : MonoBehaviour {
+	
+	public float boing;
+	private Rigidbody2D myRigid;
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other){
+		
+		if(other.gameObject.tag == "Player"){
+			myRigid = other.GetComponent<CharController>().rigidbody2D;
+			other.GetComponent<CharController>().rigidbody2D.velocity = new Vector2(myRigid.velocity.x,boing);
+			audio.Play ();
+		}
+	}
+}
